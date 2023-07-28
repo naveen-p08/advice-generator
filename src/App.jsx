b33465386a60bc30fd46e87ce2655a942a3f687e
Naveen <naveen1808@live.in>
2023-07-28 23:19:21 +0530

    advice container styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 function App() {
-  const number = 5;
-  return <></>;
+  const idNumber = 5;
+  const advice =
+    "For every complex problem there is an answer that is clear, simple, and wrong.";
+  return (
+    <>
+      <div className={"advice-container"}>
+        <p className={"number"}>
+          Advice <span className={"highlight"}>#{idNumber}</span>
+        </p>
+        <p className={"advice-text"}>{advice}</p>
+      </div>
+    </>
+  );
 }
 
 export default App;

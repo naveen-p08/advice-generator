90b934c0267bd0b1c18adf2f2f6f57002d67aeb6
Naveen <naveen1808@live.in>
2023-07-29 00:51:26 +0530

    added button disabled state

--- src/App.jsx
+++ src/App.jsx
@@ -52,6 +52,7 @@ function App() {
         className={"fetch-button"}
         onClick={handleClick}
         disabled={isLoading}
+        autoFocus={isLoading}
       >
         <span className={"visually-hidden"}>Get Advice</span>
         <img src="./images/icon-dice.svg" alt="dice icon" aria-hidden={true} />

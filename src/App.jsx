fe7767b12bee2281eef3a65fb4294ec04ee60b1d
Naveen <naveen1808@live.in>
2023-07-29 00:30:27 +0530

    added button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+
 function App() {
-  const idNumber = 5;
-  const advice =
-    "For every complex problem there is an answer that is clear, simple, and wrong.";
+  const [advice, setAdvice] = useState("Press the Button to get a advice");
+  const [idNumber, setIdNumber] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const URL = "https://api.adviceslip.com/advice";
+
+  async function handleClick() {
+    try {
+      setIsLoading(true);
+      const res = await fetch(URL);
+      const data = await res.json();
+      setAdvice(data.slip.advice);
+      setIdNumber(data.slip.id);
+      setIsLoading(false);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <>
       <div className={"advice-container"}>
         <p className={"number"}>
-          Advice <span className={"highlight"}>#{idNumber}</span>
+          {idNumber === 0 ? (
+            " "
+          ) : (
+            <>
+              Advice <span className={"highlight"}>#{idNumber}</span>
+            </>
+          )}
         </p>
         <p className={"advice-text"}>{advice}</p>
       </div>
@@ -24,7 +48,11 @@ function App() {
         </picture>
       </div>
 
-      <button className={"fetch-button"}>
+      <button
+        className={"fetch-button"}
+        onClick={handleClick}
+        disabled={isLoading}
+      >
         <span className={"visually-hidden"}>Get Advice</span>
         <img src="./images/icon-dice.svg" alt="dice icon" aria-hidden={true} />
       </button>
